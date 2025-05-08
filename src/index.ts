5b471edb4b8e13d44403bf0102a553d9fa25abde
kobaken <kentafly88@gmail.com>
2025-05-09 06:00:21 +0900

    Added graphql endpoint by yoga

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,35 @@
 import { Hono } from 'hono'
+import { createSchema, createYoga } from 'graphql-yoga'
 
-const app = new Hono()
+type Bindings = {
+  NODE_ENV: string;
+}
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono<{Bindings: Bindings}>()
+
+const schema = createSchema({
+  typeDefs: /* GraphQL */ `
+    type Query {
+      hello: String!
+    }
+  `,
+  resolvers: {
+    Query: {
+      hello: () => 'Hello world!',
+    }
+  }
+});
+
+app.on(['GET', 'POST'], '/graphql', async (c) => {
+  const yoga = createYoga({
+    graphiql: c.env.NODE_ENV === 'development' ? {
+      title: 'My GraphiQL',
+      defaultQuery: `query { hello }`,
+    } : undefined,
+    schema
+  });
+
+  return await yoga.fetch(c.req.raw, c.env, c.executionCtx);
+});
 
 export default app
