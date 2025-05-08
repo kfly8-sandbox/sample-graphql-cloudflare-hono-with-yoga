d79fef06263a1b78a43b22eb04a68ca7441324ab
kobaken <kentafly88@gmail.com>
2025-05-09 06:14:02 +0900

    use pothos

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono'
-import { createSchema, createYoga } from 'graphql-yoga'
+import { createYoga } from 'graphql-yoga'
+import SchemaBuilder from '@pothos/core';
 
 type Bindings = {
   NODE_ENV: string;
@@ -7,19 +8,17 @@ type Bindings = {
 
 const app = new Hono<{Bindings: Bindings}>()
 
-const schema = createSchema({
-  typeDefs: /* GraphQL */ `
-    type Query {
-      hello: String!
-    }
-  `,
-  resolvers: {
-    Query: {
-      hello: () => 'Hello world!',
-    }
-  }
+const builder = new SchemaBuilder({});
+
+builder.queryType({
+  fields: (t) => ({
+    hello: t.string({
+      resolve: () => 'Hello, World',
+    }),
+  }),
 });
 
+const schema = builder.toSchema();
 
 app.on(['GET', 'POST'], '/graphql', async (c) => {
   const yoga = createYoga({ schema });
